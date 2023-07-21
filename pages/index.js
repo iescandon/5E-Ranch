62cc4e1e958707d506e2766101aec37e7aee109c
iescandon <iescan4@gmail.com>
2023-07-20 21:51:30 -0500

    changed heroimage

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,16 @@ import getContent from '@/utils/getContent'
 export default function Home({content}) {
   return (
     <main>
-      <div className='relative top-0 left-0 sm:h-[250px] md:h-[350px] lg:h-[450px]'>
-      {/* {content[0].fields.heroImage.fields.file.url} */}
-      {/* {content[0].fields.otherPics[0].fields.file.url} */}
-        <img src={content[0].fields.otherPics[0].fields.file.url} className='h-full w-full object-cover object-center'/>
-        {/* <div className='absolute inset-0 bg-gradient-to-b from-transparent to-black from-5%'> */}
-        <div className='absolute inset-0 bg-gradient-to-b from-transparent to-black flex flex-row items-center justify-center'>
-          <img src={content[0].fields.logos[7].fields.file.url} className='h-[100px] md:h-[200px] lg:h-[300px] drop-shadow-2xl opacity-50'/>
+      <div className='relative top-0 left-0 sm:h-[250px] md:h-[350px] lg:h-[550px]'>
+        <img src={content[0].fields.heroImage.fields.file.url} className='h-full w-full object-cover'/>
+        {/* <div className='absolute inset-0 flex flex-row items-end justify-start'> */}
+        <div className='absolute inset-0 bg-gradient-to-b from-transparent to-black from-40% flex flex-row items-center justify-center'>
+          {/* <img src={content[0].fields.logos[5].fields.file.url} className='h-[150px] md:h-[250px] lg:h-[250px] drop-shadow-2xl'/> */}
         </div>
       </div>
-      <div className='h-[600px] bg-black'></div>
-      {/* <div className="h-[400px] bg-white bg-gradient-to-b from-black to-white"></div> */}
+      <div className='flex flex-row justify-center h-[400px] bg-black'>
+        {/* <img src={content[0].fields.logos[5].fields.file.url} className='h-[150px] md:h-[250px] lg:h-[350px] opacity-70'/> */}
+      </div>
     </main>
   )
 }
