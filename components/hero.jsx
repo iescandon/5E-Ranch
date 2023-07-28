10491a4c040b7f6b48af8342e98cee95cd7b57ca
iescandon <iescan4@gmail.com>
2023-07-27 22:02:49 -0500

    added subtitle and button

--- components/hero.jsx
+++ components/hero.jsx
@@ -4,15 +4,11 @@ export default function Hero({content}) {
     <section>
       <div className='relative top-0 left-0 h-[500px] md:h-[450px] lg:h-[600px]'>
         <img src={content[0].fields.otherPics[5].fields.file.url} className='h-full w-full object-cover'/>
-        {/* <div className='absolute inset-0 flex flex-row items-end justify-start'> */}
-        {/* <div className='absolute inset-0 flex flex-col items-center justify-center'> */}
         <div className="absolute inset-0 flex flex-row justify-center m-4">
           <img src={content[0].fields.logos[5].fields.file.url}  alt="" className="absolute h-[80px] lg:h-[100px]"/>
         </div>
         <div className='absolute inset-0 bg-gradient-to-b from-transparent to-black from-30% flex flex-col items-center justify-center'>
-          {/* <img src={content[0].fields.logos[7].fields.file.url} className='h-[150px] md:h-[250px] lg:h-[250px] drop-shadow-2xl'/> */}
-          <p className="text-3xl md:text-4xl lg:text-5xl text-white font-extrabold text-center drop-shadow-md font-jakarta !leading-[1.25] w-[350px] md:w-[600px] lg:w-[800px]">Bringing naturally and ethically raised beef to your table</p>
-          {/* <button className="bg-yellow-400 p-3">LEARN MORE</button> */}
+          <h1 className="text-3xl md:text-4xl lg:text-5xl text-white font-extrabold text-center drop-shadow-md font-jakarta !leading-[1.25] w-[350px] md:w-[600px] lg:w-[800px]">Bringing naturally and ethically raised beef to your table</h1>
         </div>
       </div>
     </section>
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,21 @@
-// import Image from 'next/image'
-// import { Inter } from 'next/font/google'
 import Hero from '@/components/hero';
 import Nav from '@/components/nav';
 import getContent from '@/utils/getContent'
-
-// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({content}) {
   return (
     <main>
       {/* <Nav content={content} /> */}
       <Hero content={content} />
-      {/* <div className='flex flex-row justify-center h-[400px]'> */}
       <div className='relative flex flex-col-reverse md:flex-row justify-center w-full px-4 py-8 md:p-6 lg:p-8'>
         <div className='w-full pt-8 md:pt-0 md:w-1/2'>
           <img src={content[0].fields.otherPics[24].fields.file.url} className=""/>
         </div>
         <div className='relative w-full md:w-1/2 md:pl-8'>
-          <p className='text-xl md:text-2xl lg:text-3xl font-bold font-jakarta pb-2'>Naturally Better</p>
-          <p className='text-sm font-jakarta'>The best option for beef for your family, comes from a fresh locally grown farm. 5E Ranch provides the best in beef and wagyu beef in Texas.</p>
-          {/* <div className="absolute border-b-[16px] border-r-[16px] border-black w-2/3 h-2/3 right-0 bottom-0 hidden md:block"></div> */}
+          <h3 className='text-xs lg:text-sm font-jakarta'>JAPANESE WAGYU BEEF</h3>
+          <h2 className='text-xl md:text-2xl lg:text-3xl font-bold font-jakarta pb-4'>Naturally Better</h2>
+          <p className='text-sm font-jakarta pb-6'>The best option for beef for your family, comes from a fresh locally grown farm. 5E Ranch provides the best in beef and wagyu beef in Texas.</p>
+          <button className="bg-black text-white p-3">LEARN MORE</button>
         </div>
       </div>
       {/* <div className='bg-black h-[150px] w-full flex flex-row justify-center'>
@@ -37,3 +33,4 @@ Home.getInitialProps = async () => {
   const content = await getContent("homePageContent");
   return { content };
 };
+
