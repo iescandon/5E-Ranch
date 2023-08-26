9daddadc5dedc9123a0272378a61c48d566010e7
iescandon <iescan4@gmail.com>
2023-08-25 23:18:08 -0500

    removed api version

--- src/pages/api/checkout_sessions.js
+++ src/pages/api/checkout_sessions.js
@@ -1,8 +1,6 @@
 import { Stripe } from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2020-08-27",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
